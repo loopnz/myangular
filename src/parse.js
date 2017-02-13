d75739c465c4468c94d7521c881222cecff9852d
cyzhang <zcy1234>
2017-02-13 15:54:14 +0800

    parse

--- src/parse.js
+++ src/parse.js
@@ -9,6 +9,11 @@ function Lexer() {
 
 }
 
+Lexer.prototype.is = function(chs) {
+
+    return chs.indexOf(this.ch) >= 0;
+};
+
 Lexer.prototype.lex = function(text) {
     this.text = text;
     this.index = 0;
@@ -17,15 +22,31 @@ Lexer.prototype.lex = function(text) {
     while (this.index < this.text.length) {
         this.ch = this.text.charAt(this.index);
         if (this.isNumber(this.ch) ||
-            (this.ch === '.' && this.isNumber(this.peek()))) {
+            (this.is('.') && this.isNumber(this.peek()))) {
             this.readNumber();
-        } else if (this.ch === "'" || this.ch == "\"") {
+        } else if (this.is('\'"')) {
             this.readString(this.ch);
+        } else if (this.is('[],{}:')) {
+            this.tokens.push({
+                text: this.ch
+            });
+            this.index++;
+        } else if (this.isIdent(this.ch)) {
+            this.readIdent();
+        } else if (this.isWhitespace(this.ch)) {
+            this.index++;
         } else {
             throw 'Unexpected next character:' + this.ch;
         }
     }
     return this.tokens;
+};
+Lexer.prototype.isWhitespace = function(ch) {
+    return ch === ' ' || ch === '\r' || ch === '\t' || ch === '\n' || ch === '\v' || ch === '\u00a0';
+};
+Lexer.prototype.isIdent = function(ch) {
+
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch === '_' || ch === '$';
 };
 Lexer.prototype.isNumber = function(ch) {
     return '0' <= ch && ch <= '9';
@@ -37,6 +58,21 @@ Lexer.prototype.peek = function() {
 Lexer.prototype.isExpOperator = function(ch) {
     return ch === '-' || ch === '+' || this.isNumber(ch);
 };
+Lexer.prototype.readIdent = function() {
+    var text = '';
+    while (this.index < this.text.length) {
+        var ch = this.text.charAt(this.index);
+        if (this.isIdent(ch) || this.isNumber(ch)) {
+            text += ch;
+        } else {
+            break;
+        }
+        this.index++;
+    }
+    var token = { text: text, identifier: true };
+    this.tokens.push(token);
+};
+
 Lexer.prototype.readNumber = function() {
     var number = '';
     while (this.index < this.text.length) {
@@ -64,42 +100,50 @@ Lexer.prototype.readNumber = function() {
     });
 
 };
-var ESCAPES={
-	'n':"\n",
-	'f':"\f",
-	"r":"\r",
-	"t":"\t",
-	"v":"\v",
-	"'":"'",
-	"\"":"\""
+var ESCAPES = {
+    'n': "\n",
+    'f': "\f",
+    "r": "\r",
+    "t": "\t",
+    "v": "\v",
+    "'": "'",
+    "\"": "\""
 };
 Lexer.prototype.readString = function(quote) {
     this.index++;
     var string = "";
-    var escape=false;
+    var escape = false;
     while (this.index < this.text.length) {
 
         var ch = this.text.charAt(this.index);
-        if (escape){
-        	var replacement=ESCAPES[ch];
-        	if(replacement){
-        		string+=replacement;
-        	}else{
-        		string+=ch;
-        	}
-        	escape=false;
-        }
-        else if(ch === quote) {
+        if (escape) {
+            if (ch === 'u') {
+                var hex = this.text.substring(this.index + 1, this.index + 5);
+                if (!hex.match(/[\da-f]{4}/i)) {
+                    throw 'Invalid unicode escape';
+                }
+                this.index += 4;
+                string += String.fromCharCode(parseInt(hex, 16));
+            } else {
+                var replacement = ESCAPES[ch];
+                if (replacement) {
+                    string += replacement;
+                } else {
+                    string += ch;
+                }
+            }
+
+            escape = false;
+        } else if (ch === quote) {
             this.index++;
             this.tokens.push({
                 text: string,
                 value: string
             });
             return;
-        }else if(ch==="\\"){
-        	escape=true;
-        } 
-        else {
+        } else if (ch === "\\") {
+            escape = true;
+        } else {
             string += ch;
 
         }
@@ -114,6 +158,17 @@ function AST(lexer) {
 
 AST.Program = 'Program';
 AST.Literal = 'Literal';
+AST.ArrayExpression = 'ArrayExpression';
+AST.ObjectExpression = 'ObjectExpression';
+AST.Property = 'Property';
+AST.Identifier = 'Identifier';
+AST.ThisExpression='ThisExpression';
+AST.prototype.constants = {
+    'null': { type: AST.Literal, value: null },
+    'true': { type: AST.Literal, value: true },
+    'false': { type: AST.Literal, value: false },
+    'this': {type:AST.ThisExpression}
+};
 
 AST.prototype.ast = function(text) {
     this.tokens = this.lexer.lex(text);
@@ -124,14 +179,91 @@ AST.prototype.program = function() {
 
     return {
         type: AST.Program,
-        body: this.constant()
+        body: this.primary()
     };
+};
+
+AST.prototype.primary = function() {
+    if (this.expect('[')) {
+        return this.arrayDeclaration();
+    } else if (this.expect('{')) {
+        return this.object();
+    } else if (this.constants.hasOwnProperty(this.tokens[0].text)) {
+        return this.constants[this.consume().text];
+    } else if(this.peek().identifier){
+        return this.identifier();
+    } else {
+        return this.constant();
+    }
+
+};
+
+AST.prototype.object = function() {
+    var properties = [];
+    if (!this.peek('}')) {
+        do {
+            var property = { type: AST.Property };
+            if (this.peek().identifier) {
+                property.key = this.identifier();
+            } else {
+                property.key = this.constant();
+            }
+            this.consume(":");
+            property.value = this.primary();
+            properties.push(property);
+        } while (this.expect(','));
+    }
+    this.consume('}');
+    return { type: AST.ObjectExpression, properties: properties };
+};
+
+AST.prototype.identifier = function() {
+    return { type: AST.Identifier, name: this.consume().text };
+};
+
+AST.prototype.arrayDeclaration = function() {
+    var elements = [];
+    if (!this.peek(']')) {
+        do {
+            if (this.peek(']')) {
+                break;
+            }
+            elements.push(this.primary());
+        } while (this.expect(','));
+    }
+    this.consume(']');
+    return { type: AST.ArrayExpression, elements: elements };
+};
+
+AST.prototype.consume = function(e) {
+    var token = this.expect(e);
+    if (!token) {
+        throw 'Unexpected. Expecting:' + e;
+    }
+    return token;
+};
+
+AST.prototype.expect = function(e) {
+    var token = this.peek(e);
+    if (token) {
+        return this.tokens.shift();
+    }
+};
+
+
+AST.prototype.peek = function(e) {
+    if (this.tokens.length > 0) {
+        var text = this.tokens[0].text;
+        if (text === e || !e) {
+            return this.tokens[0];
+        }
+    }
 };
 
 AST.prototype.constant = function() {
     return {
         type: AST.Literal,
-        value: this.tokens[0].value
+        value: this.consume().value
     };
 };
 
@@ -141,14 +273,15 @@ function ASTCompiler(astBuilder) {
 }
 
 ASTCompiler.prototype.compile = function(text) {
-
     var ast = this.astBuilder.ast(text);
     this.state = {
-        body: []
+        body: [],
+        nextId:0,
+        vars:[]
     };
     this.recurse(ast);
     /*jshint -W054*/
-    return new Function(this.state.body.join(''));
+    return new Function('s',(this.state.vars.length?'var '+this.state.vars.join(',')+';':'')+this.state.body.join(''));
     /*jshint +W054*/
 };
 
@@ -159,19 +292,58 @@ ASTCompiler.prototype.recurse = function(ast) {
             break;
         case AST.Literal:
             return this.escape(ast.value);
+        case AST.ArrayExpression:
+            var elements = _.map(ast.elements, function(element) {
+                return this.recurse(element);
+            }, this);
+            return '[' + elements.join(',') + ']';
+        case AST.ObjectExpression:
+            var properties = _.map(ast.properties, function(property) {
+                var key = property.key.type === AST.Identifier ?
+                    property.key.name :
+                    this.escape(property.key.value);
+                var value = this.recurse(property.value);
+                return key + ":" + value;
+            }, this);
+            return '{' + properties.join(',') + '}';
+        case AST.Identifier:
+            var intoId=this.nextId();
+            this.if_('s',this.assign('v0',this.nonComputedMember('s',ast.name)));
+            return intoId;
+        case AST.ThisExpression:
+            return 's';
     }
 };
-
-ASTCompiler.prototype.escape=function(value){
-	if(_.isString(value)){
-		return '\''+value.replace(this.stringEscapeRegex,this.stringEscapeFn)+'\'';
-	}else{
-		return value;
-	}
+ASTCompiler.prototype.nonComputedMember=function(left,right){
+    return '('+left+').'+right;
 };
-ASTCompiler.prototype.stringEscapeRegex=/[^ a-zA-Z0-9]/g;
-ASTCompiler.prototype.stringEscapeFn=function(c){
-	return '\\u'+('0000'+c.charCodeAt(0).toString(16)).slice(-4);
+ASTCompiler.prototype.if_=function(test,consequent){
+    this.state.body.push('if(',test,'){',consequent,'}');
+};
+
+ASTCompiler.prototype.assign=function(id,value){
+    return id+"="+value+";";
+};
+
+
+ASTCompiler.prototype.nextId=function(){
+    var id='v'+(this.state.nextId++);
+    this.state.vars.push(id);
+    return id;
+};
+
+ASTCompiler.prototype.escape = function(value) {
+    if (_.isString(value)) {
+        return '\'' + value.replace(this.stringEscapeRegex, this.stringEscapeFn) + '\'';
+    } else if (_.isNull(value)) {
+        return 'null';
+    } else {
+        return value;
+    }
+};
+ASTCompiler.prototype.stringEscapeRegex = /[^ a-zA-Z0-9]/g;
+ASTCompiler.prototype.stringEscapeFn = function(c) {
+    return '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4);
 };
 
 function Parser(lexer) {
@@ -181,5 +353,6 @@ function Parser(lexer) {
 }
 
 Parser.prototype.parse = function(text) {
+
     return this.astCompiler.compile(text);
 };
--- test/parse_spec.js
+++ test/parse_spec.js
@@ -50,27 +50,102 @@ describe('parse', function() {
     });
 
     it('解析单引号的字符串', function() {
-    	var fn=parse("'abc'");
-    	expect(fn()).toEqual('abc');
+        var fn = parse("'abc'");
+        expect(fn()).toEqual('abc');
     });
     it('解析双引号的字符串', function() {
-    	var fn=parse('"abc"');
-    	expect(fn()).toEqual("abc");
+        var fn = parse('"abc"');
+        expect(fn()).toEqual("abc");
     });
 
-    it('引号不匹配时抛出异常', function() {
-    	expect(function(){
-    		parse('"abc\'');
-    	}).toThrow();
+   it('引号不匹配时抛出异常', function() {
+        expect(function() {
+            parse('"abc\'');
+        }).toThrow();
     });
 
     it('能够解析表达式内部的单引号', function() {
-    	var fn=parse("'a\\\'b'");
-    	expect(fn()).toEqual('a\'b');
+        var fn = parse("'a\\\'b'");
+        expect(fn()).toEqual('a\'b');
     });
 
-        it('能够解析表达式内部的双引号', function() {
-    	var fn=parse('"a\\\"b"');
-    	expect(fn()).toEqual('a\"b');
+    it('能够解析表达式内部的双引号', function() {
+        var fn = parse('"a\\\"b"');
+        expect(fn()).toEqual('a\"b');
+    });
+
+    it('解析字符串中unicode字符', function() {
+        var fn = parse("'\\u00A0'");
+        expect(fn()).toEqual("\u00A0");
+    });
+    it('不解析字符串中的非法unicode字符', function() {
+        expect(function() {
+            parse('"\\u00T0"');
+        }).toThrow();
+    });
+
+    it('解析null', function() {
+        var fn = parse('null');
+        expect(fn()).toBe(null);
+    });
+
+    it('解析true', function() {
+        var fn = parse("true");
+        expect(fn()).toBe(true);
+    });
+    it('解析false', function() {
+        var fn = parse('false');
+        expect(fn()).toBe(false);
+    });
+
+    it('忽视空格(whitespace)', function() {
+        var fn = parse(" \n42 ");
+        expect(fn()).toEqual(42);
+    });
+
+    it('解析空数组', function() {
+        var fn = parse('[]');
+        expect(fn()).toEqual([]);
+    });
+
+    it('解析非空数组', function() {
+        var fn = parse('[1,"two",[3],true]');
+        expect(fn()).toEqual([1, "two", [3], true]);
+    });
+
+    it('解析数组忽略最后一个逗号分隔符', function() {
+        var fn=parse("[1,2,3,]");
+        expect(fn()).toEqual([1,2,3]);
+    });
+    it('解析空对象', function() {
+        var fn=parse('{}');
+        expect(fn()).toEqual({});
+    });
+    it('解析非空对象', function() {
+        var fn=parse('{"a Key":1,\'another-key\':2}');
+        expect(fn()).toEqual({'a Key':1,'another-key':2});
+    });
+
+    it('解析非空对象(对象的key是标识符,没有引号)', function() {
+        var fn=parse('{a:1,b:[2,3],c:{d:4}}');
+        expect(fn()).toEqual({a:1,b:[2,3],c:{d:4}});
+    });
+
+    it('解析属性名,从scope中查找属性', function() {
+        var fn=parse('aKey');
+        expect(fn({aKey:43})).toBe(43);
+        expect(fn({})).toBeUndefined();
+    });
+
+    it('未传入scope时返回undefined', function() {
+        var fn=parse('aKey');
+        expect(fn()).toBeUndefined();
+    });
+
+    it('解析表达式中的this', function() {
+        var fn=parse('this');
+        var scope={};
+        expect(fn(scope)).toBe(scope);
+        expect(fn()).toBeUndefined();
     });
 });
